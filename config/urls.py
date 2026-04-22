902046af025bd51ecc7937cd60a591e7c0a5bcf6
Souvik Sinha <svksnha@gmail.com>
2026-04-23 00:34:54 +0530

    Add responsive landing page and improve API presentation

--- config/urls.py
+++ config/urls.py
@@ -19,11 +19,12 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView 
 
-
+from config.views import home
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
     path('api/', include('issues.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
